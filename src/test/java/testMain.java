89f699c0d48d0a6f60a1f0ffd7dcbd8b32665896
Adyel Ullahil Mamun <adyel@live.com>
2018-09-28 13:15:39 +0600

    Update DB working

--- src/test/java/testMain.java
+++ src/test/java/testMain.java
@@ -1,12 +1,20 @@
 import model.Movie;
 import model.Response;
+import okhttp3.Dispatcher;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import util.DBConnect;
 import util.tmdbAPI;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class testMain {
 
@@ -18,6 +26,9 @@ public class testMain {
 //        System.out.println(movie.getTitle() + "(" + movie.getVoteAverage() + ") - " + movie.getOverview());
 //        System.out.println(movie.getGenreIds());
 
+
+        Dispatcher dispatcher = new Dispatcher(Executors.newFixedThreadPool(20));
+        dispatcher.setMaxRequests(20);
 
         Retrofit retrofit = new Retrofit.Builder()
                                 .baseUrl("https://api.themoviedb.org")
@@ -31,21 +42,38 @@ public class testMain {
         Call<Response> result = apiInterface.response(3,"search","909af28891cff6bfe9ea2fea81fc5426","Oldboy", 2013);
 
 
-        result.enqueue(new Callback<Response>() {
-            @Override
-            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
-                Response responseList = response.body();
-                List<Movie> movieList = responseList.getMovies();
-                System.out.println(movieList.get(0).getGenreIds());
+//        result.enqueue(new Callback<Response>() {
+//            @Override
+//            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+//                Response responseList = response.body();
+//                List<Movie> movieList = responseList.getMovies();
+//                System.out.println(movieList.get(0).getGenreIds());
+//
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Response> call, Throwable t) {
+//                t.printStackTrace();
+//
+//            }
+//        });
 
 
-            }
+        try {
+           retrofit2.Response response = result.execute();
+           Response reply = (Response) response.body();
+           List<Movie> movieList = reply.getMovies();
+           Movie firstMovie = movieList.get(0);
 
-            @Override
-            public void onFailure(Call<Response> call, Throwable t) {
-                t.printStackTrace();
+           Connection connection = DBConnect.getConnection();
+           Statement statement = connection.createStatement();
+           statement.executeUpdate("UPDATE Movie_List SET Rating = " + firstMovie.getPopularity() + ", Overview = '" + firstMovie.getOverview() + "' WHERE ID = 4415");
 
-            }
-        });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
