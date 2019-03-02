236a4519830e6167041200764a40d765b858bbac
Adyel <adyel@live.com>
2019-03-02 18:37:46 +0600

    many to many implemented

--- src/main/java/model/orm/Genre.java
+++ src/main/java/model/orm/Genre.java
@@ -1,9 +1,7 @@
 package model.orm;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "genre")
@@ -23,4 +21,44 @@ public class Genre {
 
     @Column(name = "genre_name")
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
+    @JoinTable(name = "movie_genre",
+            joinColumns = @JoinColumn(name = "genre_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id")
+    )
+    private List<MovieDetails> movies;
+
+
+
+
+
+    // INFO: Getters & Setters
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<MovieDetails> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<MovieDetails> movies) {
+        this.movies = movies;
+    }
 }
